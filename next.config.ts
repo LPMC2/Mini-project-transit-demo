6b6844967a3ccf88159c74f69efef5db5de3dcb8
LPMC2 <liamwong200310@gmail.com>
2025-08-26 00:10:59 +0800

    g

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev','http://169.254.225.148'],
 };
-
 export default nextConfig;
